bd2c3a47e9a60548344eb44f2b43d84970f2572a
scorpio2024 <scorpio2003@naver.com>
2023-12-19 00:13:43 +0900

    Mod: (for) add item (eight)

--- python/For.py
+++ python/For.py
@@ -1,3 +1,3 @@
-numbers = ["one", "two", "three", "for", "five", "six", "seven"]
+numbers = ["one", "two", "three", "for", "five", "six", "seven", "eight"]
 for number in numbers:
     print(number)
