84ed77df646a98cecd8e231d61910f984387de2c
scorpio2024 <scorpio2003@naver.com>
2023-12-18 23:16:45 +0900

    Mod: (While) type annotation 적용

--- python/While.py
+++ python/While.py
@@ -1,6 +1,6 @@
 tree_hit: int = 0
 while tree_hit < 10:
     tree_hit = tree_hit + 1
-    print("나무를 %d번 찍었습니다." % tree_hit)
+    print(f"나무를 {tree_hit}번 찍었습니다.")
     if tree_hit == 10:
         print("나무 넘어갑니다.")
